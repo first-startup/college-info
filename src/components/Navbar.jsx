99d44c317f3d725bd0e9b4cd1d6355abcd9ad5af
Chintan0539 <chintanpatel0539@gmail.com>
2022-07-04 10:45:14 +0530

    navbar added revision needed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./css/Navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper container">
+        <h2 className="nav-logo">LOGO</h2>
+
+        <input
+          type="search"
+          class="search__input"
+          placeholder="Search for the college"
+        />
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
