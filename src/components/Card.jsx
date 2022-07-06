cb202f08f0e8e4947dfcdeb81da3edcd05df0cf7
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2022-07-06 15:49:15 +0530

    Card:Map n Props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./css/Card.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="card-image">
         <img
-          src="https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&w=800"
-          alt=""
+          src={props.src}
+          alt={props.name}
         />
       </div>
       <div className="card-content">
-        <h2>Chintan's startup</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          enim natus expedita nostrum quam incidunt.
-        </p>
+        <h2 className="card-name">{props.name}</h2>
+        <p className="card-desc">{props.description}</p>
         <div className="card-tags">
-          <div className="tag">sales</div>
-          <div className="tag">manufacturing</div>
+          <div className="tag">{props.tag1}</div>
+          <div className="tag">{props.tag2}</div>
         </div>
       </div>
     </div>
--- src/components/CardWrap.jsx
+++ src/components/CardWrap.jsx
@@ -2,15 +2,41 @@ import React from "react";
 import Card from "./Card";
 
 const CardWrap = () => {
+  const startups = [
+    { id: 1, 
+      name: "Ayush's Startup",
+      src: "https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&w=800", 
+      tag1: "tech", 
+      tag2: "business", 
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore enim natus expedita nostrum quam incidunt."
+    },
+    { id: 2, 
+      name: "Chintan's Startup",
+      src: "https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&w=800", 
+      tag1: "sales", 
+      tag2: "manufacturing", 
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore enim natus expedita nostrum quam incidunt."
+    },
+    { id: 3, 
+      name: "Ayush's Startup",
+      src: "https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&w=800", 
+      tag1: "tech", 
+      tag2: "business", 
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore enim natus expedita nostrum quam incidunt."
+    },
+    { id: 4, 
+      name: "Chintan's Startup",
+      src: "https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&w=800", 
+      tag1: "sales", 
+      tag2: "manufacturing", 
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore enim natus expedita nostrum quam incidunt."
+    },
+  ];
   return (
     <div className="cardwrap">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {startups.map((startup) => (
+        <Card key={startup.id} {...startup} />
+      ))}
     </div>
   );
 };
